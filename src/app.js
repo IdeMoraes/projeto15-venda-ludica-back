d78ea10160986cf94aa0253e597f9cc12eb51d4e
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-23 02:16:49 -0300

    Ajustes iniciais: .env .env.example .gitignore .package.json e pastas

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express, {json} from 'express';
+import chalk from 'chalk';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import router from './routers/index.js';
+
+dotenv.config();
+const porta = process.env.PORTA || 5000;
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.use(router);
+
+app.listen(porta, ()=>{
+    console.log(`Listening on ${chalk.blue(porta)}`);
+});
