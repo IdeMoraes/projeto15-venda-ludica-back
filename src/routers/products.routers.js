2a646f8facc70089cec53fb6b94811d281e95df9
nandasino <fernandamsino@gmail.com>
2022-11-23 22:17:39 -0300

    get products pronto

--- src/routers/products.routers.js
+++ src/routers/products.routers.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {createProduct} from "../controllers/products.controller.js";
+import {createProduct, getProducts} from "../controllers/products.controller.js";
+import { authValidation } from "../middlewares/authValidation.js";
 import { createProductValidation } from "../middlewares/createProductValidation.js";
 
 const router = Router();
 
 router.post("/products", createProductValidation , createProduct);
+router.get("/products", authValidation , getProducts);
 
 export default router;
