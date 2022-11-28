1ecea928fcc42a870c9d353ec46893449da016ca
Igor de Moraes <r.de.moraes.igor@gmail.com>
2022-11-28 15:36:14 -0300

    Faz o post /orders

--- src/controllers/orders.controller.js
+++ src/controllers/orders.controller.js
@@ -4,24 +4,21 @@ import {ordersCollection} from "../db.js";
 export async function postOrder(req, res){
     const newOrder = req.body;
     const user = req.user;
-/*     {
-        userID:
-        products: [{productId: ###, quantity: ###} , {productId: ###, quantity: ###}],
-        adress: {country: ##, state: ##, city: ##, area: ##, street: ##, zipCode: ##, houseNumber: ##, complement: ##},
-        recipient: {name: ##, phoneNumber: ##},
-        paymentMethod: ##
-        } */
+
     try {
         const cart = await ordersCollection.findOne({userId: user._id.toString()});
         if(!cart){
-            console.log("Primeira adição ao carrinho");
-            await cartCollection.insertOne({userId: user._id.toString(), products: [newProductToCart]});
-            res.sendStatus(201);
+            alert("Adicione ao carrinho antes de tentar finalizar o pedido.");
+            return;
         }
-        else{
-            await cartCollection.updateOne({userId: user._id.toString()}, {$push: {products: newProductToCart}});
-            res.sendStatus(201);
-        }
+        await ordersCollection.insertOne({
+            userId: user._id.toString(), 
+            products: cart.products, 
+            adress: newOrder.adress, 
+            recipient: newOrder.recipient,
+            payment: newOrder.payment
+        });
+        res.sendStatus(201);
     } catch (error) {
         res.sendStatus(500);
     }
